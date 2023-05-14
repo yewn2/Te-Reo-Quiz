469e9f5567d39010e94b3f72c01a6124cf77841d
yewn2 <yewn2@middleton.school.nz>
2023-05-14 19:53:53 +1200

    game_mode version 3

--- 05_game_mode_v3.py
+++ 05_game_mode_v3.py
@@ -9,20 +9,20 @@ def mode():
             "Location Words)\n"
     modes = ["numbers", "colours", "location words"]
     valid = False
-    game_mode = ""
+    game_mode_1 = ""
 
     # keep asking until valid mode is entered
     while not valid:
         try:
             # ask for game mode
-            game_mode = str(input("The game modes available are:\n"
+            game_mode_1 = str(input("The game modes available are:\n"
                                   "\t(1) Numbers\n"
                                   "\t(2) Colours\n"
                                   "\t(3) Location Words\n"
                                   "What game mode would you like to play? ")).lower()
             print()
-            if game_mode != modes[0] and game_mode != modes[1] and \
-               game_mode != modes[2]:
+            if game_mode_1 != modes[0] and game_mode_1 != modes[1] and \
+               game_mode_1 != modes[2]:
                 valid = False
             else:
                 valid = True
@@ -34,8 +34,10 @@ def mode():
                 # set and display game mode according to one of the three
                 # possible answers
                 print("You have set the game mode to {}."
-                      .format(game_mode.title()))
+                      .format(game_mode_1.title()))
+                return game_mode_1
 
 
 # main routine
-mode()
+game_mode = mode()
+print(game_mode)
