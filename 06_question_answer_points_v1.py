48243fcce22d04f1e9df4f7e950bc1604aa96f3a
yewn2 <yewn2@middleton.school.nz>
2023-05-16 11:46:02 +1200

    question_answer_points version 1 (unfinished)

---
+++ 06_question_answer_points_v1.py
@@ -0,0 +1,12 @@
+"""
+First version of a component which will first pick a random question from 10
+options, then check that the user's answer is correct, then award points
+according whether the answer is correct or not. This then repeats 5 times to
+make a quiz.
+"""
+
+
+# Imports
+import random
+
+# lists of possible terms
